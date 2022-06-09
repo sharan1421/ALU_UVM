d52eb8e5a0168bd6a1b7324ce98013bd48a2a483
sharan1421 <74460444+sharan1421@users.noreply.github.com>
2022-06-09 18:00:11 +0530

    Create alu_package.sv

---
+++ tb_files/alu_package.sv
@@ -0,0 +1,13 @@
+package alu_pkg;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "agent.sv"
+`include "coverage.sv"
+`include "environment.sv"
+`include "test.sv"
+endpackage
